6bc1bae6dce74f8f61d899d34c483dfe5745a617
Tiago Luchini <tiago@luchini.nyc>
2021-12-08 20:43:18 -0500

    Simplify specialized env configuration approach

--- src/system_utils/initializer.clj
+++ src/system_utils/initializer.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [ambiente.core :as ambiente]
             [clojure.java.io :as io]
+            [clojure.walk :refer [postwalk] :as w]
             [edamame.core :as edn]
             [integrant.core :as ig]))
 
@@ -21,17 +22,23 @@
           slurp
           (edn/parse-string {:readers readers})))
 
+(defn ^:private placeholder-replace [m x]
+  (postwalk (fn [i] (if (symbol? i)
+                      (get x i)
+                      i))
+            m))
+
 (defn read-config []
   (let [base (-> "base.edn" config-resource read-file)
         env (or (ambiente/env :env) default-env)
-        config-file (str env  ".edn")
-        config (-> config-file config-resource read-file)
-        merged-config (merge base config)]
+        specialized-file (str env  ".edn")
+        specialized (-> specialized-file config-resource read-file)
+        merged-config (placeholder-replace base specialized)]
     (when (nil? base)
       (throw (ex-info (str "Unable to load base config: base.edn")
                       {:anomaly/category ::invalid-system-config})))
-    (when (nil? config)
-      (throw (ex-info (str "Unable to load config: " config-file)
+    (when (nil? specialized)
+      (throw (ex-info (str "Unable to load specialized config: " specialized-file)
                       {:anomaly/category ::invalid-system-config})))
     (when (nil? merged-config)
       (throw (ex-info (str "Unable to merge config")
