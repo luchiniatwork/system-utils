0e7dcbc11d3758e3782c089d914d79fbeabbb969
Tiago Luchini <tiago@luchini.nyc>
2021-12-13 13:06:25 -0500

    Add an edn parser for more complex env vars

--- src/system_utils/env.clj
+++ src/system_utils/env.clj
@@ -1,5 +1,6 @@
 (ns system-utils.env
   (:require [ambiente.core :as ambiente]
+            [edamame.core :as edn]
             [integrant.core :as ig]
             [taoensso.timbre :refer [debug info warn error fatal report]]))
 
@@ -7,3 +8,10 @@
   (info {:msg "Initializing Environment Variable"
          :var var})
   (ambiente/env var))
+
+(defmethod ig/init-key ::edn-parsed-var [_ {:keys [var]}]
+  (info {:msg "Initializing EDN-parsed Environment Variable"
+         :var var})
+  (-> var
+      ambiente/env
+      edn/parse-string))
